af3d8a1209618fe1d4da4f07f182145445e3dbe6
Dmytro <278596441+dmytro-f1@users.noreply.github.com>
2026-04-23 10:24:58 +0300

    Pinia initialization

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import "./style.css"
-
 import App from "./App.vue"
-import { createApp } from "vue"
 
-createApp(App).mount("#app")
+import { createApp } from "vue"
+import { createPinia } from "pinia"
+
+const pinia = createPinia()
+
+createApp(App).use(pinia).mount("#app")
