cf6a7b21151a42f6b4b0f0f40f295ff672f1e5fc
Dmytro <278596441+dmytro-f1@users.noreply.github.com>
2026-04-23 12:05:57 +0300

    I18N initialization

---
+++ src/i18n/index.ts
@@ -0,0 +1,13 @@
+import { createI18n } from "vue-i18n"
+
+import en from "./locales/en.json"
+import uk from "./locales/uk.json"
+
+const i18n = createI18n({
+  legacy: false,
+  locale: "en",
+  fallbackLocale: "en",
+  messages: { en, uk },
+})
+
+export default i18n
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import App from "./App.vue"
 
 import { createApp } from "vue"
 import { createPinia } from "pinia"
+import i18n from "./i18n"
 
 const pinia = createPinia()
 
-createApp(App).use(pinia).mount("#app")
+createApp(App).use(pinia).use(i18n).mount("#app")
