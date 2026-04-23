67e994970a445031090e174e9591e640f86ac922
Dmytro <278596441+dmytro-f1@users.noreply.github.com>
2026-04-23 10:20:08 +0300

    App layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MonitoringSidebar from "./components/sidebar/MonitoringSidebar.vue"
+</script>
 
 <template>
-  <div>Hello world</div>
+  <div class="flex h-screen p-4 gap-4 bg-gray-50">
+    <aside
+      class="w-75 border bg-white border-gray-300 rounded-lg overflow-hidden"
+    >
+      <MonitoringSidebar />
+    </aside>
+    <main class="flex-1 border bg-white border-gray-300 rounded-lg">
+      <!-- Map -->
+    </main>
+  </div>
 </template>
---
+++ src/components/sidebar/MonitoringSidebar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SidebarHeader from "./SidebarHeader.vue"
+import SidebarList from "./SidebarList.vue"
+</script>
+
+<template>
+  <SidebarHeader></SidebarHeader>
+  <SidebarList></SidebarList>
+</template>
